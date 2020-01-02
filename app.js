785fb637f68b4622b751e40560696136d9234422
Bojo <bozhidar_zahariev@hotmail.com>
2020-01-02 15:40:53 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -2,69 +2,123 @@ const MePic = props => {
   return <img id='mePic' alt='me' src='images/me.jpg' />;
 };
 
-/*
-class Frame0 extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      init: true
-    };
-  }
-
-  render() {
+const Frame0 = props => {
+  if (props.frameStatus) {
     return (
-      <div className='frames'>
-        <p className={`framesText ${this.state.init && 'framesTextUpLeft'}`}>Projects</p>
+      <div className={`frames ${props.frameStatus}`} onClick={props.onClick}>
+        <p className={`framesText ${props.textStatus}`}>Projects</p>
+        <div className='projects'>1</div>
+        <div className='projects'>1</div>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={`frames ${props.isHovered === 0 ? 'framesHovered0' : false}`}
+        onMouseEnter={props.onMouseEnter}
+        onMouseLeave={props.onMouseLeave}
+        onClick={props.onClick}>
+        <p className={`framesText ${props.textStatus}`}>Projects</p>
       </div>
     );
   }
-}
-*/
-const Frame0 = props => {
-  return (
-    <div className={`frames ${props.frameStatus}`} onClick={props.onClick}>
-      <p className={`framesText ${props.textStatus}`}>Projects</p>
-    </div>
-  );
 };
 
 const Frame1 = props => {
-  return (
-    <div className={`frames ${props.frameStatus}`} onClick={props.onClick}>
-      <p className={`framesText ${props.textStatus}`}>About me</p>
-    </div>
-  );
+  if (props.frameStatus) {
+    return (
+      <div className={`frames ${props.frameStatus}`} onClick={props.onClick}>
+        <p className={`framesText ${props.textStatus}`}>About me</p>
+        <div className='projects'>1</div>
+        <div className='projects'>1</div>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={`frames ${props.isHovered === 1 ? 'framesHovered1' : false}`}
+        onMouseEnter={props.onMouseEnter}
+        onMouseLeave={props.onMouseLeave}
+        onClick={props.onClick}>
+        <p className={`framesText ${props.textStatus}`}>About me</p>
+      </div>
+    );
+  }
 };
 
 const Frame2 = props => {
-  return (
-    <div className={`frames ${props.frameStatus}`} onClick={props.onClick}>
-      <p className={`framesText ${props.textStatus}`}>CV</p>
-    </div>
-  );
+  if (props.frameStatus) {
+    return (
+      <div className={`frames ${props.frameStatus}`} onClick={props.onClick}>
+        <p className={`framesText ${props.textStatus}`}>CV</p>
+        <div className='projects'>1</div>
+        <div className='projects'>1</div>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={`frames ${props.isHovered === 2 ? 'framesHovered1' : false}`}
+        onMouseEnter={props.onMouseEnter}
+        onMouseLeave={props.onMouseLeave}
+        onClick={props.onClick}>
+        <p className={`framesText ${props.textStatus}`}>CV</p>
+      </div>
+    );
+  }
 };
 
 const Frame3 = props => {
-  return (
-    <div className={`frames ${props.frameStatus}`} onClick={props.onClick}>
-      <p className={`framesText ${props.textStatus}`}>Get in touch</p>
-    </div>
-  );
+  if (props.frameStatus) {
+    return (
+      <div className={`frames ${props.frameStatus}`} onClick={props.onClick}>
+        <p className={`framesText ${props.textStatus}`}>Get in Touch</p>
+        <div className='projects'>1</div>
+        <div className='projects'>1</div>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={`frames ${props.isHovered === 3 ? 'framesHovered1' : false}`}
+        onMouseEnter={props.onMouseEnter}
+        onMouseLeave={props.onMouseLeave}
+        onClick={props.onClick}>
+        <p className={`framesText ${props.textStatus}`}>Get in Touch</p>
+      </div>
+    );
+  }
 };
 
 class Container extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: false
+      clicked: false,
+      isHovered: false
     };
 
     this.clickControl = this.clickControl.bind(this);
+    this.handleFramesHover = this.handleFramesHover.bind(this);
+    this.handleFramesHoverLeave = this.handleFramesHoverLeave.bind(this);
   }
 
   clickControl(fr) {
     this.setState({
-      clicked: fr
+      clicked: fr,
+      isHovered: false
+    });
+  }
+
+  handleFramesHover(fr) {
+    this.setState({
+      isHovered: fr
+    });
+  }
+
+  handleFramesHoverLeave() {
+    this.setState({
+      isHovered: false
     });
   }
 
@@ -73,10 +127,34 @@ class Container extends React.Component {
       return (
         <div className='main'>
           <MePic />
-          <Frame0 onClick={() => this.clickControl('framesClicked0')} textStatus={'framesTextUpLeft'} />
-          <Frame1 onClick={() => this.clickControl('framesClicked1')} textStatus={'framesTextUpRight'} />
-          <Frame2 onClick={() => this.clickControl('framesClicked2')} textStatus={'framesTextDownLeft'} />
-          <Frame3 onClick={() => this.clickControl('framesClicked3')} textStatus={'framesTextDownRight'} />
+          <Frame0
+            onMouseEnter={() => this.handleFramesHover(0)}
+            onMouseLeave={this.handleFramesHoverLeave}
+            isHovered={this.state.isHovered}
+            onClick={() => this.clickControl('framesClicked0')}
+            textStatus={'framesTextUpLeft'}
+          />
+          <Frame1
+            onMouseEnter={() => this.handleFramesHover(1)}
+            onMouseLeave={this.handleFramesHoverLeave}
+            isHovered={this.state.isHovered}
+            onClick={() => this.clickControl('framesClicked1')}
+            textStatus={'framesTextUpRight'}
+          />
+          <Frame2
+            onMouseEnter={() => this.handleFramesHover(2)}
+            onMouseLeave={this.handleFramesHoverLeave}
+            isHovered={this.state.isHovered}
+            onClick={() => this.clickControl('framesClicked2')}
+            textStatus={'framesTextDownLeft'}
+          />
+          <Frame3
+            onMouseEnter={() => this.handleFramesHover(3)}
+            onMouseLeave={this.handleFramesHoverLeave}
+            isHovered={this.state.isHovered}
+            onClick={() => this.clickControl('framesClicked3')}
+            textStatus={'framesTextDownRight'}
+          />
         </div>
       );
     }
